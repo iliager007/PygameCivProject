e69ad79c830d5bf74ff624a5752f9626d423369f
shelk <shelk04@mail.ru>
2020-01-28 11:56:24 +0300

    Город

---
+++ town.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Town:
+
+    def __init__(self, x, y, name):
+        """Города - основная единица страны"""
+        self.x = x
+        self.y = y
+        font = pygame.font.Font(None, 50)
+
+    def initTown(self):
+        pass
